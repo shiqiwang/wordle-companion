e347355cc2f9e368571d9cc5b82b756d07c876e7
shiqiwang <shiqi.wang@live.com>
2022-01-25 16:36:26 +0800

    header

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import './header.scss';
+
+function Header() {
+  return <header className="header">
+    <div className="title">WORDLE COMPANION</div>
+  </header>
+}
+
+export default Header;
